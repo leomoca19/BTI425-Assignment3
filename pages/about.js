37c9deae0864848b188530b4e6c441452e07dc1d
Leonardo Caceres <leomoca19@gmail.com>
2025-02-04 16:02:48 -0500

    complete step 5

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,36 @@
-export default function About() {
-  return <p>About</p>
+import Link from 'next/link'
+import Card from 'react-bootstrap/Card'
+import ListingDetails from '@/components/ListingDetails'
+import PageHeader from '@/components/PageHeader'
+
+const url = 'https://listings-api-eight.vercel.app/api/listings/1206363'
+
+export async function getStaticProps() {
+  const data = await fetch(url).then((res) => res.json())
+  return { props: { listing: data } }
+}
+
+export default function About({ listing }) {
+  return (
+    <>
+      <PageHeader text="About the Developer - Leonardo de la Mora Caceres" />
+      <Card>
+        <Card.Body>
+          <p>
+            Hi, I am a software developer student, this is my first published
+            project using react and next for web development
+          </p>
+
+          <p>
+            This is the place I want you to visit: &nbsp;
+            <Link href={`/listing/${listing._id}`} passHref legacyBehavior>
+              <a>"{listing.name}"</a>
+            </Link>
+            &nbsp;(Airbnb)
+          </p>
+        </Card.Body>
+        <ListingDetails listing={listing} />
+      </Card>
+    </>
+  )
 }
