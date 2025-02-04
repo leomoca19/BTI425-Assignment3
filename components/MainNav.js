ad2a8bf82f91353a93c4f55c98a07f56d87a3cdb
Leonardo Caceres <leomoca19@gmail.com>
2025-02-03 21:47:38 -0500

    implement main navbar

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,3 +1,25 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Link from 'next/link'
+
 export default function MainNav() {
-  return <p>MainNav</p>
+  return (
+    <>
+      <Navbar expand="lg" className="navbar-dark bg-dark fixed-top">
+        <Container>
+          <Navbar.Brand>Leonardo de la Mora Caceres</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link href="/" passHref legacyBehavior>
+              <Nav.Link>Listings</Nav.Link>
+            </Link>
+            <Link href="/about" passHref legacyBehavior>
+              <Nav.Link>About</Nav.Link>
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
+    </>
+  )
 }
