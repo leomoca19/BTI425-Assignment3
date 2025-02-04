64e36c8a106b8aec5c7f5b11335118d937a9596a
Leonardo Caceres <leomoca19@gmail.com>
2025-02-04 16:03:23 -0500

    on step 6

--- components/ListingDetails.js
+++ components/ListingDetails.js
@@ -1,3 +1,28 @@
-export default function ListingDetails() {
-  return <p>ListingDetails</p>
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function ListingDetails({ listing }) {
+  console.log(listing.picture_url)
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg>
+            <img
+              onError={(event) => {
+                event.target.onerror = null
+                event.target.src =
+                  'https://placehold.co/600x400?text=Photo+Not+Available'
+              }}
+              className="img-fluid w-100"
+              src={listing.picture_url}
+              alt="Listing Image"
+            />
+            <br />
+            <br />
+          </Col>
+          <Col lg></Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
