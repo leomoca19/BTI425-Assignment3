8998295ac790fb979a45c8182757210697c76926
Leonardo Caceres <leomoca19@gmail.com>
2025-02-05 17:26:54 -0500

    finish ste 8

--- pages/listing/[id].js
+++ pages/listing/[id].js
@@ -1,3 +1,28 @@
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+import Error from 'next/error'
+import ListingDetails from '@/components/ListingDetails'
+import PageHeader from '@/components/PageHeader'
+import { domain } from '@/path'
+
 export default function Listing() {
-  return <p>Listing by Id</p>
+  const router = useRouter()
+  const { id } = router.query
+
+  const { data, error, isLoading } = useSWR(`${domain}/api/listings/${id}`)
+
+  if (isLoading) return null
+
+  if (error) {
+    return <Error statusCode={404} />
+  } else {
+    if (data) {
+      return (
+        <>
+          <PageHeader text={data?.name} />
+          <ListingDetails listing={data} />
+        </>
+      )
+    } else return <Error statusCode={404} />
+  }
 }
